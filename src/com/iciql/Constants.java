d32c1f4d1eccbe0b9dd7c557d52879c8ec01069a
James Moger <james.moger@gmail.com>
2011-08-04 13:51:26 -0400

    Change identifiers for next release.

--- src/com/iciql/Constants.java
+++ src/com/iciql/Constants.java
@@ -25,15 +25,15 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "0.6.0-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "2011-08-03";
+	public static final String VERSION_DATE = "PENDING";
 	
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String API_CURRENT = "1";
+	public static final String API_CURRENT = "2";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
