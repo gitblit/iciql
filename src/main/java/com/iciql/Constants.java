fef63be10e793d0f551bdede7205e58829738d61
James Moger <james.moger@gitblit.com>
2014-11-05 08:51:34 -0500

    Bump to 1.4.0-SNAPSHOT and update documentation

--- src/main/java/com/iciql/Constants.java
+++ src/main/java/com/iciql/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "1.3.1-SNAPSHOT";
+	public static final String VERSION = "1.4.0-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
