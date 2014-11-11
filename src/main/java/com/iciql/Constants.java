1245e4db2dadcc7b10e9e516eafb58a944be0dc6
James Moger <james.moger@gitblit.com>
2014-11-10 21:53:53 -0500

    Reset build identifiers for next development cycle

--- src/main/java/com/iciql/Constants.java
+++ src/main/java/com/iciql/Constants.java
@@ -25,11 +25,11 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "1.5.0";
+	public static final String VERSION = "1.6.0-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "2014-11-10";
+	public static final String VERSION_DATE = "PENDING";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
